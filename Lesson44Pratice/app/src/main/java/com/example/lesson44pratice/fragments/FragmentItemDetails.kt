2e81eac609f022b64acedfa2614fba6522a2365a
wilfred gicharu <wilfredgicharu254@gmail.com>
2022-04-11 23:25:45 +0300

    click

--- Lesson44Pratice/app/src/main/java/com/example/lesson44pratice/fragments/FragmentItemDetails.kt
+++ Lesson44Pratice/app/src/main/java/com/example/lesson44pratice/fragments/FragmentItemDetails.kt
@@ -7,18 +7,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.lesson44pratice.Model.Data
 import com.example.lesson44pratice.R
 
 
-class FragmentItemDetails : Fragment() {
+class FragmentItemDetails : Fragment(R.layout.fragment_item_details) {
     lateinit var iv_detail_icon: ImageView
     lateinit var tv_detail_name: TextView
     lateinit var tv_detail_creator: TextView
     lateinit var tv_detail_description: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        iv_detail_icon = view.findViewById(R.id.iv_detail_icon)
+        tv_detail_name = view.findViewById(R.id.tv_detail_name)
+        tv_detail_creator = view.findViewById(R.id.tv_detail_creator)
+        tv_detail_description = view.findViewById(R.id.tv_detail_description)
+        setup()
+    }
+
+    private fun setup() {
+        val idFromArgs = arguments?.getLong(Long::class.java.canonicalName)
+
+        idFromArgs?.let { id ->
+            val details = Data.itemDetails.find { it.id == id }
+
+            details?.let {
+                iv_detail_icon.setImageResource(details.icon)
+                tv_detail_name.text = details.name
+                tv_detail_creator.text = details.creator
+                tv_detail_description.text = details.description
+            }
+        }
     }
 
     override fun onCreateView(
